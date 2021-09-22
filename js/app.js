d7269aede095449fa7943cf9034ed0cd7769cacf
ManuelNunja <81265139+ManuelNunja@users.noreply.github.com>
2021-09-22 18:21:31 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let separator = "-".repeat(50);
+// DESAFIO 02 ----------------------------------------------------------------------------------------------
+/*let separator = "-".repeat(50);
 console.log(separator);
 alert("Bienvenido al sistema de consultas");
 let user_prename = prompt("Nombres");
@@ -55,5 +56,59 @@ if(validate){
     console.log(`Codigo de visita: ${codeGen}`);
     console.log(`Tiempo aprox de respuesta: ${xHrs}`);
     console.log(separator);
+}*/
+// DESAFIO 03 ----------------------------------------------------------------------------------------------
+let separator = "-".repeat(50);
+let cantItems = 10;
+let cantItems_print = 0;
+console.log("Sistema de Consultas - Reporte");
+console.log(separator);
+alert("Bienvenido al sistema de consultas");
+let strMessage = "¿Que tipo de reporte desea visualizar? \n\n 1. Reporte simple\n 2. Reporte detallado \n 3. Fin sistema \n";
+let strDefault = "Ingrese el numero de reporte ...";
+let tipoReporte = parseInt(prompt(strMessage, strDefault)); 
+while(!Number(tipoReporte)){ // BLUCLE WHILE
+    tipoReporte = parseInt(prompt(strMessage, strDefault));
+}
+strMessage = "¿Que tipo de filtro se debe aplicar a la lista? \n\n 1. Todos \n 2. Solo EN ESPERA \n 3. Solo ATENDIDOS \n";
+strDefault = "Ingrese el codigo del filtro ...";
+let xfiltro;
+if(Number(tipoReporte)){
+    if(tipoReporte > 0 && tipoReporte <= 3){
+        let strItem = "error";
+        let strDetail = "error";
+        let strDate = "error";
+        let estadoItem = "EN ESPERA";
+        switch(tipoReporte){
+            case 1: // Reporte simple
+                xfiltro = parseInt(prompt(strMessage, strDefault));
+                for (i = 0;i < cantItems; i++){
+                    if(i == 3 || i == 9){estadoItem = "ATENDIDO";}else{estadoItem="EN ESPERA";}
+                    strItem = (i + 1) + " | Nombre Usuario | " + "VISITA" + (i + 1) + " | " + estadoItem + "\n";
+                    if(xfiltro == 1){console.log(strItem);cantItems_print++}
+                    if(xfiltro == 2 && estadoItem == "EN ESPERA"){console.log(strItem);cantItems_print++}
+                    if(xfiltro == 3 && estadoItem == "ATENDIDO"){console.log(strItem);cantItems_print++}
+                }
+                break;
+            case 2: // Reporte detallado
+                xfiltro = parseInt(prompt(strMessage, strDefault));
+                for (i = 0;i < cantItems; i++){
+                    if(i == 3 || i == 9){estadoItem = "ATENDIDO";}else{estadoItem="EN ESPERA";}
+                    strItem = (i + 1) + " | Nombre Usuario | " + "VISITA" + (i + 1) + " | " + estadoItem + "\n";
+                    strDetail = "\t consulta de usuario \n"
+                    strDate = "\t Fecha de consulta: 01/01/2021 \n";
+                    if(xfiltro == 1){console.log(strItem + strDetail + strDate);cantItems_print++}
+                    if(xfiltro == 2 && estadoItem == "EN ESPERA"){console.log(strItem + strDetail + strDate);cantItems_print++}
+                    if(xfiltro == 3 && estadoItem == "ATENDIDO"){console.log(strItem + strDetail + strDate);cantItems_print++}
+                }
+                break;
+            case 3: // Fin dsistema
+                console.log("Fin sistema.")
+                break;
+        }
+        console.log("Cantidad de registros: " + cantItems_print);
+    }
+}else{
+    console.log("ERROR DE INGRESO DE DATOS");
 }
 
